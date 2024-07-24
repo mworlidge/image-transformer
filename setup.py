2112c82c7184d52ec1fc24fa5cf805555072641b
mworlidge <48948223+mworlidge@users.noreply.github.com>
2024-07-24 11:50:36 +0100

    Python package set up

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='image_transformer',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy==1.24.4',
+        'opencv-python==4.3.0.38',
+    ],
+    extras_require={
+        'dev': [
+            'black==23.7.0',
+            'flake8==6.1.0',
+        ],
+    },
+    test_requires=[
+        'pytest',
+    ],
+    author='Tilly Worlidge',
+    author_email='matilda.worlidge@gmail.com',
+    description='A Python package for sampling non-overlapping regions from an image.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/mworlidge/image-transformer.git",
+)
